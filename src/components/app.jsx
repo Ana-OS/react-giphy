f802c2259c7b48c4ed7eb38a5f977f7babf307c8
ana sanots <anasoliveiraaa@gmail.com>
2021-02-12 22:35:34 +0000

    search feature added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 import SearchBar from './search_bar.jsx'
 import Gif from './gif.jsx'
+import GifList from './gifs_list.jsx'
   // component , render and props are built in React
 class App extends Component {
+    constructor(props) {
+      super(props);
+      // states é tudo o que é passivel de mudar
+      this.state = {
+        gifs: [],
+        selectedGifId: "2qFAkEKypfbu0Sgw0d"
+      }
+      // this.search("homer thinking")
+    }
+
+    search = (query) => {
+      giphy("B0AnJ5suZRu9SI9HN1xuOGqqbrz4hWLN").search({
+        q: query,
+        limit: 3
+      }, (err, res) => {
+        // passamos para o empty array gifs que definimos no constructor o resultado dado pela search -data
+          this.setState({
+            gifs: res.data
+          })
+      });
+    }
+
   render(){  // este render vai fazer ponte com reactDOM.render de index
+    // const gifs = [
+    //   {id: "2qFAkEKypfbu0Sgw0d"},
+    //   {id: "Icv8n1Y8gnPigLEBgk"}
+    // ];
+
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search}/>
             <div className="selected-gif">
-              <Gif />
+              <Gif id={this.state.selectedGifId}/>
             </div>
         </div>
-        <div className="rigth-scene"></div>
+        <div className="rigth-scene">
+          <GifList gifs={this.state.gifs}/>
+        </div>
       </div>
     )
   }
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 
 class Gif extends Component {
   render() {
-    const src = "https://media2.giphy.com/media/2qFAkEKypfbu0Sgw0d/giphy.gif?cid=ecf05e4780185a8229cb65e80f78fae7ea0487474e90a167&rid=giphy.gif";
+    // o props.id é devolvido como props por app. É de lá que vem a info de que este componente precisa ""
+
+    const src = `https://media2.giphy.com/media/${this.props.id}/giphy.gif?cid=ecf05e4780185a8229cb65e80f78fae7ea0487474e90a167&rid=giphy.gif`;
 
     return (
       <img src={src} alt="" className="gif"/>
