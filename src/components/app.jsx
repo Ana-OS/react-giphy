7f9d0c8da85a55a895d0a7076416b5c5fe273773
ana sanots <anasoliveiraaa@gmail.com>
2021-02-12 19:48:14 +0000

    css added

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+  // component , render and props are built in React
+class App extends Component {
+  render(){
+    return (
+
+      <div>
+        <div className="left-scene"></div>
+        <div className="rigth-scene"></div>
+      </div>
+    )
+  }
+
+}
+
+export default App;
